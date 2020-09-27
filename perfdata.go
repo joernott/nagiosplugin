e68e95df1195614c52069e48f649f6b904500e2f
Rémi Ferrand <remi.ferrand@cc.in2p3.fr>
2020-09-27 19:33:49 +0200

    Adapt to my personal need

--- perfdata.go
+++ perfdata.go
@@ -6,16 +6,49 @@ import (
 	"strconv"
 )
 
+type PerfDatumValue interface {
+	fmt.Stringer
+}
+
+type FloatPerfDatumValue float64
+
+func NewFloatPerfDatumValue(f float64) (FloatPerfDatumValue, error) {
+	if math.IsInf(f, 0) || math.IsNaN(f) {
+		return FloatPerfDatumValue(f), fmt.Errorf("Perfdata value may not be infinity or NaN: %v.", f)
+	}
+	return FloatPerfDatumValue(f), nil
+}
+
+func (f FloatPerfDatumValue) String() string {
+	return fmtPerfFloat(float64(f))
+}
+
+type UndeterminedPerfDatumValue struct{}
+
+const (
+	// value may be a literal "U" instead
+	// this would indicate that the actual value couldn't be determined
+	UndeterminedPerfDatumStr = "U"
+)
+
+func NewUndeterminedPerfDatumValue() UndeterminedPerfDatumValue {
+	return UndeterminedPerfDatumValue{}
+}
+
+func (u UndeterminedPerfDatumValue) String() string {
+	return UndeterminedPerfDatumStr
+}
+
 // PerfDatum represents one metric to be reported as part of a check
 // result.
 type PerfDatum struct {
 	label string
-	value float64
+	value PerfDatumValue
 	unit  string
 	min   *float64
 	max   *float64
-	warn  *float64
-	crit  *float64
+	warn  *Range
+	crit  *Range
 }
 
 // fmtPerfFloat returns a string representation of n formatted in the
@@ -32,60 +65,39 @@ func fmtPerfFloat(n float64) string {
 // Zero to four thresholds may be supplied: min, max, warn and crit.
 // Thresholds may be positive infinity, negative infinity, or NaN, in
 // which case they will be omitted in check output.
-func NewPerfDatum(label string, unit string, value float64, thresholds ...float64) (*PerfDatum, error) {
+func NewPerfDatum(label string, unit string, value PerfDatumValue, warn, crit *Range, min, max *float64) (*PerfDatum, error) {
 	datum := new(PerfDatum)
 	datum.label = label
 	datum.value = value
 	datum.unit = unit
-	if math.IsInf(value, 0) || math.IsNaN(value) {
-		return nil, fmt.Errorf("Perfdata value may not be infinity or NaN: %v.", value)
-	}
-	if len(thresholds) >= 1 {
-		datum.min = &thresholds[0]
-	}
-	if len(thresholds) >= 2 {
-		datum.max = &thresholds[1]
-	}
-	if len(thresholds) >= 3 {
-		datum.warn = &thresholds[2]
-	}
-	if len(thresholds) >= 4 {
-		datum.crit = &thresholds[3]
-	}
+	datum.warn = warn
+	datum.crit = crit
 	return datum, nil
-}
-
-// isThresholdSet returns true if one of min, max, warn or crit are set
-// and false otherwise. They are determined to be 'set' if they are not
-// a) the nil pointer, b) infinity (positive or negative) or c) NaN.
-func isThresholdSet(t *float64) bool {
-	switch {
-	case t == nil:
-		return false
-	case math.IsInf(*t, 0):
-		return false
-	case math.IsNaN(*t):
-		return false
-	}
-	return true
-}
-
-// fmtThreshold returns a string representation of min, max, warn or
-// crit (whether or not they are set).
-func fmtThreshold(t *float64) string {
-	if !isThresholdSet(t) {
-		return ""
-	}
-	return fmtPerfFloat(*t)
 }
 
 // String returns the string representation of a PerfDatum, suitable for
 // check output.
 func (p PerfDatum) String() string {
-	val := fmtPerfFloat(p.value)
-	value := fmt.Sprintf("'%s'=%s%s", p.label, val, p.unit)
-	value += fmt.Sprintf(";%s;%s", fmtThreshold(p.warn), fmtThreshold(p.crit))
-	value += fmt.Sprintf(";%s;%s", fmtThreshold(p.min), fmtThreshold(p.max))
+	value := fmt.Sprintf("'%s'=%s%s", p.label, p.value.String(), p.unit)
+
+	var warn, crit string
+	if p.warn != nil {
+		warn = p.warn.String()
+	}
+	if p.crit != nil {
+		crit = p.crit.String()
+	}
+
+	var min, max string
+	if p.min != nil {
+		min = fmtPerfFloat(*p.min)
+	}
+	if p.max != nil {
+		max = fmtPerfFloat(*p.max)
+	}
+
+	value += fmt.Sprintf(";%s;%s", warn, crit)
+	value += fmt.Sprintf(";%s;%s", min, max)
 	return value
 }
 
